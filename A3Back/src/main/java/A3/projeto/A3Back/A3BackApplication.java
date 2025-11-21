5f96ee435734a484af47cc4ff9d8dc76bacae850
MTsunaM <lucas45abel@outlook.com>
2025-11-20 22:04:36 -0300

    Login e cadastro de empresas adicionados

--- A3Back/src/main/java/A3/projeto/A3Back/A3BackApplication.java
+++ A3Back/src/main/java/A3/projeto/A3Back/A3BackApplication.java
@@ -2,8 +2,10 @@ package A3.projeto.A3Back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class A3BackApplication {
 
 	public static void main(String[] args) {
